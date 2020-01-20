2498202da08d74cc36ab0b4f97f1ab222c026582
pighaddt <pighaddt@gmail.com>
2020-01-20 15:27:28 +0800

    Real Time EMG Accuracy & RMS data  finished

--- app/src/main/java/com/itri/accuracyemg3x3/MainActivity.java
+++ app/src/main/java/com/itri/accuracyemg3x3/MainActivity.java
@@ -13,9 +13,7 @@ import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
-import com.github.mikephil.charting.utils.EntryXComparator;
 
-import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,10 +34,15 @@ public class MainActivity extends AppCompatActivity {
         btn3x3 = findViewById(R.id.button_3x3);
 
         lineChartAccuracySetting();
+        lineChartRMSSetting();
 
         LineData lineDataAccuracy = new  LineData();
         lineDataAccuracy.setValueTextColor(Color.BLACK);
         lineChartAccuracy.setData(lineDataAccuracy);
+
+        LineData lineDataRMS = new  LineData();
+        lineDataRMS.setValueTextColor(Color.BLACK);
+        lineChartRMS.setData(lineDataRMS);
 
 
 
@@ -49,23 +52,44 @@ public class MainActivity extends AppCompatActivity {
         legendAccuracy.setForm(Legend.LegendForm.CIRCLE);
         legendAccuracy.setTextColor(Color.BLUE);
 
-        XAxis xAxis = lineChartAccuracy.getXAxis();
-        xAxis.setTextColor(Color.BLACK);
-        xAxis.setAvoidFirstLastClipping(true);
+        Legend legendRMS = lineChartRMS.getLegend();
+        legendRMS.setForm(Legend.LegendForm.LINE);
+        legendRMS.setTextColor(Color.BLACK);
 
-        YAxis yAxis = lineChartAccuracy.getAxisLeft();
-        yAxis.setTextColor(Color.BLACK);
-        yAxis.setDrawGridLines(true);
-        yAxis.setAxisMinimum(0f);
-        yAxis.setAxisMaximum(100f);
+        XAxis xAxisAccuracy = lineChartAccuracy.getXAxis();
+        xAxisAccuracy.setTextColor(Color.BLACK);
+        xAxisAccuracy.setAvoidFirstLastClipping(true);
 
-        YAxis leftAxis = lineChartAccuracy.getAxisRight();
-        leftAxis.setEnabled(false);
+        XAxis xAxisRMS = lineChartRMS.getXAxis();
+        xAxisRMS.setTextColor(Color.BLUE);
+        xAxisRMS.setAvoidFirstLastClipping(true);
+
+        YAxis yAxisAccuracy = lineChartAccuracy.getAxisLeft();
+        yAxisAccuracy.setTextColor(Color.BLACK);
+        yAxisAccuracy.setDrawGridLines(true);
+        yAxisAccuracy.setAxisMinimum(0f);
+        yAxisAccuracy.setAxisMaximum(100f);
+
+        YAxis yAxisRMS = lineChartRMS.getAxisLeft();
+        yAxisRMS.setTextColor(Color.BLUE);
+        yAxisRMS.setDrawGridLines(true);
+        yAxisRMS.setAxisMinimum(0f);
+        yAxisRMS.setAxisMaximum(100f);
+
+        YAxis leftAxisAccuracy = lineChartAccuracy.getAxisRight();
+        leftAxisAccuracy.setEnabled(false);
+
+        YAxis leftAxisRMS = lineChartRMS.getAxisRight();
+        leftAxisRMS.setEnabled(false);
 
         lineChartAccuracy.setDrawBorders(true);
         lineChartAccuracy.getXAxis().setDrawGridLines(true);
-//        chartAccuracyStartPlot();
+
+        lineChartRMS.setDrawBorders(true);
+        lineChartRMS.getXAxis().setDrawGridLines(true);
     }
+
+
 
     private void lineChartAccuracySetting() {
         lineChartAccuracy.setNoDataText("No Data available");
@@ -80,7 +104,18 @@ public class MainActivity extends AppCompatActivity {
         lineChartAccuracy.setBackgroundColor(Color.WHITE);
     }
 
-
+    private void lineChartRMSSetting() {
+        lineChartRMS.setNoDataText("No Data available");
+        lineChartRMS.getDescription().setEnabled(true);
+        lineChartRMS.getDescription().setText("Real Time RMS data  ");
+//        lineChartRMS.setDescription("Real Time EMG Accuracy Plot  (%) ");//2.1.5
+        lineChartRMS.setTouchEnabled(false); // 設定是否可以觸控
+        lineChartRMS.setDragEnabled(true); // 是否可以拖拽
+        lineChartRMS.setScaleEnabled(false); // 是否可以縮放 x和y軸, 預設是true
+        lineChartRMS.setDrawGridBackground(true); // 背景網格
+        lineChartRMS.setPinchZoom(false); //設定x軸和y軸能否同時縮放。預設是否
+        lineChartRMS.setBackgroundColor(Color.WHITE);
+    }
 
 
 
@@ -96,7 +131,7 @@ public class MainActivity extends AppCompatActivity {
                         @Override
                         public void run () {
                             addAccuracyEntry();
-//                            addRMSEntry();
+                            addRMSEntry();
                         }
                     });
                     try {
@@ -110,6 +145,7 @@ public class MainActivity extends AppCompatActivity {
         }).start();
     }
 
+
     private void addAccuracyEntry(){
         LineData dataAccuracy = lineChartAccuracy.getData();
         if (dataAccuracy != null){
@@ -118,7 +154,7 @@ public class MainActivity extends AppCompatActivity {
                 setAccuracy = createSetAccuracy();
                 dataAccuracy.addDataSet(setAccuracy);
             }
-            int random = (int)(Math.random()*100+1);
+            int random = 100 - (int)(Math.random()*100+1);
             dataAccuracy.addEntry(new Entry(setAccuracy.getEntryCount(), random), 0);
             lineChartAccuracy.notifyDataSetChanged();
 
@@ -132,7 +168,24 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void addRMSEntry() {
+        LineData dataRMS = lineChartRMS.getData();
+        if (dataRMS != null){
+            LineDataSet setRMS = (LineDataSet) dataRMS.getDataSetByIndex(0);
+            if (setRMS == null){
+                setRMS = createSetRMS();
+                dataRMS.addDataSet(setRMS);
+            }
+            int random = (int)(Math.random()*100+1);
+            dataRMS.addEntry(new Entry(setRMS.getEntryCount(), random), 0);
+            lineChartRMS.notifyDataSetChanged();
 
+            dataRMS.notifyDataChanged();
+            lineChartRMS.setVisibleXRange(100,100);
+            lineChartRMS.setMaxVisibleValueCount(150);
+            lineChartRMS.moveViewToX(dataRMS.getEntryCount());
+    }
+}
 
     private LineDataSet createSetAccuracy() {
         LineDataSet set  = new LineDataSet(null,"EMG Accuracy Data");
@@ -147,4 +200,16 @@ public class MainActivity extends AppCompatActivity {
         return set;
     }
 
+    private LineDataSet createSetRMS() {
+        LineDataSet set  = new LineDataSet(null,"RMS Data");
+        set.setAxisDependency(YAxis.AxisDependency.LEFT);
+        set.setLineWidth(3f);
+        set.setColor(Color.BLACK);
+        set.setHighlightEnabled(false);
+        set.setDrawValues(false);
+        set.setDrawCircles(false);
+//        set.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+        set.setCubicIntensity(1f);
+        return set;
+    }
 }
